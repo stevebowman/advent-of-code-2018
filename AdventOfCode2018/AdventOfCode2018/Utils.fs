25b503635462a460f5971de32ab580ef0101591d
Steve Bowman <Steven.Bowman@dnvgl.com>
2018-12-01 10:40:49 +0000

    Tidy up.

--- AdventOfCode2018/AdventOfCode2018/Utils.fs
+++ AdventOfCode2018/AdventOfCode2018/Utils.fs
@@ -1,11 +1,13 @@
 ﻿module Utils
 
+open System.Diagnostics
+
 let printResult result time title = 
     printf "%s Result: %A \n" title result 
     printf "%s Elapsed Time: %i ms \n" title time 
 
 let timeThis f input =
-    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let stopWatch = Stopwatch.StartNew()
     let result = f input
     stopWatch.Stop()
     result, stopWatch.ElapsedMilliseconds
