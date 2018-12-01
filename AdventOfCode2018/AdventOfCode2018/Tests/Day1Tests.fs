7ea77961be622a1968d96753fc8f3be99a1ebe57
Steve Bowman <Steven.Bowman@dnvgl.com>
2018-12-01 11:44:50 +0000

    Added NUnit tests for Day1 Part1.

---
+++ AdventOfCode2018/AdventOfCode2018/Tests/Day1Tests.fs
@@ -0,0 +1,15 @@
+﻿module Day1Tests
+
+open NUnit.Framework
+open Day1
+
+[<TestFixture>]
+type ``Day 1 Part 1``() =
+
+    [<TestCase("+1\n-2\n+3\n+1", 3)>]
+    [<TestCase("+1\n+1\n+1", 3)>]
+    [<TestCase("+1\n+1\n-2", 0)>]
+    [<TestCase("-1\n-2\n-3", -6)>]
+    member this.``can parse sample input``(input, expected) =
+        let result = day1Part1 input
+        Assert.AreEqual(expected, result) 
