f85edd5a6f2f8bbd1077a6eb2e93f5c1162710ce
Steve Bowman <Steven.Bowman@dnvgl.com>
2018-12-02 12:48:35 +0000

    Tidying up parsers.

--- AdventOfCode2018/AdventOfCode2018/Parsers.fs
+++ AdventOfCode2018/AdventOfCode2018/Parsers.fs
@@ -1,32 +1,24 @@
 ﻿module Parsers
 
 open FParsec
-open Utils
 
-let parseListOfInts input separator =
-    let listOfIntsParser = (sepBy pint32 (pchar(separator)))
-    match run listOfIntsParser input with
+let runParser parser input =
+    match run parser input with
         | Success(result, _, _)   -> result
         | Failure(errorMsg, _, _) -> 
             printfn "Failure: %s" errorMsg
             []
 
-let parseListOfStrings input separator =
+let parseListOfInts input separator =
+    let parser = (sepBy pint32 (pchar(separator)))
+    runParser parser input
 
+let parseListOfStrings input separator =
     let str : Parser<_,_> = many1Chars (noneOf [separator])
     let sep = pchar separator
     let parser = sepBy str sep
-
-    match run parser input with
-        | Success(result, _, _)   -> result
-        | Failure(errorMsg, _, _) -> 
-            printfn "Failure: %s" errorMsg
-            []
+    runParser parser input
 
 let parseStringOfChars input = 
     let parser = many anyChar
-    match run parser input with
-        | Success(result, _, _)   -> result
-        | Failure(errorMsg, _, _) -> 
-            printfn "Failure: %s" errorMsg
-            []
+    runParser parser input
