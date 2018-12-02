165b22bbffebf51082e913ab4355dfc0b4339533
Steve Bowman <Steven.Bowman@dnvgl.com>
2018-12-02 03:31:33 +0000

    Added option to choose which day to run.

--- AdventOfCode2018/AdventOfCode2018/Program.fs
+++ AdventOfCode2018/AdventOfCode2018/Program.fs
@@ -6,12 +6,21 @@ open Day1
 
 [<EntryPoint>]
 let main argv =
+
     printfn "Advent Of Code 2018"
 
-    let day1Part1Input = readFile "Input/Day1Part1.txt"
-    timeAndPrintResult day1Part1 day1Part1Input "Day 1 Part 1"
-    timeAndPrintResult day1Part2 day1Part1Input "Day 1 Part 2"
+    while true do 
 
-    Console.ReadKey() |> ignore
+        printfn "\nWhich day would you like to run? (q to quit):"
+
+        match Console.ReadLine().Trim() with
+        | "1" ->
+            let day1Part1Input = readFile "Input/Day1Part1.txt"
+            timeAndPrintResult day1Part1 day1Part1Input "Day 1 Part 1"
+            timeAndPrintResult day1Part2 day1Part1Input "Day 1 Part 2"
+    
+        | "q" | "Q" -> exit 0
+
+        | _ -> printfn "Day does not exist"
 
     0 
